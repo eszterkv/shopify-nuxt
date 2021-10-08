feb612a82cb4e652a8b5032f874e345a9feba9a3
eszter <ekov@pm.me>
2021-10-08 16:48:09 +0100

    initial commit 🦕

---
+++ components/ProductGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import getAllProducts from '../gql/get-all-products.gql'
+
+export default Vue.extend({
+  data() {
+    return {
+      products: [],
+    }
+  },
+  async fetch() {
+    try {
+      const products = await this.$apollo.query({
+        query: getAllProducts,
+      })
+      console.log(products)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <ProductGrid />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
